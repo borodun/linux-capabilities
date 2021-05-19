9d34e1c95eaa53333216a1e91ed75ebc97239d7a
Borodun <a.borodin2@g.nsu.ru>
2021-05-19 13:31:48 +0700

    deleted capNames

--- progs/capchange.c
+++ progs/capchange.c
@@ -6,46 +6,6 @@
 #include <string.h>
 #include <wait.h>
 
-const char *capNames[CAP_LAST_CAP + 1] = {
-        "chown",
-        "dac_override",
-        "dac_read_search",
-        "fowner",
-        "fsetid",
-        "kill",
-        "setgid",
-        "setuid",
-        "setpcap",
-        "linux_immutable",
-        "net_bind_service",
-        "net_broadcast",
-        "net_admin",
-        "net_raw",
-        "ipc_lock",
-        "ipc_owner",
-        "sys_module",
-        "sys_rawio",
-        "sys_chroot",
-        "sys_ptrace",
-        "sys_pacct",
-        "sys_admin",
-        "sys_boot",
-        "sys_nice",
-        "sys_resource",
-        "sys_time",
-        "sys_tty_config",
-        "mknod",
-        "lease",
-        "audit_write",
-        "audit_control",
-        "setfcap",
-        "mac_override",
-        "mac_admin",
-        "syslog",
-        "wake_alarm",
-        "block_suspend",
-        "audit_read"};
-
 int addAmbientCaps(const int *caps, int size) {
     capng_get_caps_process();
     capng_clear(CAPNG_SELECT_BOTH);
